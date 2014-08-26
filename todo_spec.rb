b3b5b784a6c5ffc65c27807a756b5472923255ea
Steven Pikelny & Tanner Blumer <pair+scyclow.tblumer3@devbootcamp.com>
2014-08-26 11:16:29 -0500

    Initialization and attribute creation

---
+++ todo_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require_relative 'todo'
+
+describe "Task" do
+  let(:task) {Task.new("title", "description")}
+  context "#initialize" do
+    it "creates a task!" do
+      expect(task).to be_an_instance_of Task
+    end
+
+    # it "creates a title for Task" do
+    # end
+  end
+
+  context "#title" do
+    it "returns the title" do
+      expect(task.title).to eq("title")
+    end
+  end
+
+  context "#description" do
+    it "returns the description" do
+      exepct(task.description).to eq("description")
+    end
+  end
+
+  context "#status" do
+    it 'defaults as Incomplete' do
+      expect(task.status).to eq("incomplete")
+    end
+  end
+
+  context "#created_at" do
+    it 'defaults to when it was initialized' do
+      expect(task.created_at).to be_an_instance_of Time
+    end
+  end
+end
