54cbb97d390873b3d7c518dbb88dc79523fde114
Steven Pikelny & Tanner Blumer <pair+scyclow.tblumer3@devbootcamp.com>
2014-08-26 14:36:45 -0500

    complete? for todo list

--- todo_spec.rb
+++ todo_spec.rb
@@ -45,7 +45,7 @@ describe "Task" do
 
   context "#mark_as_incomplete!" do
     it "changes the status of a task to incomplete" do
-      task.stub(:status) {"YOLO"}
+      task.mark_as_complete!
       task.mark_as_incomplete!
       expect(task.status).to eq("incomplete")
     end
@@ -53,13 +53,81 @@ describe "Task" do
 
   context "#complete?" do
     it "returns true when status is complete" do
-      task.stub(:status) {"complete"}
+      task.stub(:status => "complete")
       expect(task.complete?).to eq(true)
     end
 
     it "returns false when status is incomplete" do
-      task.stub(:status) {"incomplete"}
+      task.stub(:status => "incomplete")
       expect(task.complete?).to eq(false)
     end
   end
 end
+
+describe "Todo" do
+  let(:todo) {Todo.new("title")}
+
+  context "#title" do
+    it "returns the title" do
+      expect(todo.title).to eq("title")
+    end
+  end
+
+  context "#tasks" do
+    it "is an array" do
+      expect(todo.tasks).to be_an_instance_of Array
+    end
+  end
+
+  context "#add_task" do
+    it "should add a task to todo" do
+      task = double("Task")
+      todo.add_task(task)
+      expect(todo.tasks.length).to eq(1)
+    end
+  end
+
+
+#might be broken, but honestly, probably not, cause we're super cewl. Also, this comment isnt here.
+  context "#complete_all!" do
+    it "changes all tasks status' to complete" do
+      task_dummy = double("Task")
+      todo.add_task(task_dummy)
+      task_dummy.should_receive(:mark_as_complete!)
+      todo.complete_all!
+    end
+  end
+  #ask instructors, bruh
+  context "#complete?" do
+    it "returns false if any task is incomplete" do
+      5.times do
+        task_dummy = double("Task")
+        task_dummy.stub(:status => "complete")
+        task_dummy.stub(:complete? => true)
+        todo.add_task(task_dummy)
+      end
+
+      task_dummy = double("Task")
+      task_dummy.stub(:status => "incomplete")
+      task_dummy.stub(:complete? => false)
+      todo.add_task(task_dummy)
+
+      expect(todo.complete?).to eq(false)
+    end
+
+    it "returns true if all task are complete" do
+      5.times do
+        task_dummy = double("Task")
+        task_dummy.stub(:status => "complete")
+        task_dummy.stub(:complete? => true)
+        todo.add_task(task_dummy)
+      end
+
+      expect(todo.complete?).to eq(true)
+
+    end
+  end
+end
+
+
+
